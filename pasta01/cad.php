1c3890c5e3f5f9b4179ba9412380ee54cd0df7cf
Mario Santos Salles <84875639+Marinho-salles@users.noreply.github.com>
2023-05-27 16:21:58 -0300

    Feito com js

---
+++ pasta01/cad.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consultas</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <h1>Consultas</h1>
+        </header>
+        <main class="code">
+            <?php
+            $nome = $_GET["nome"];
+            $tell = ($_GET["telefone"]);
+            $doctor = ($_GET["doutor"]);
+            $date = ($_GET["data"]);
+            ?>
+        </main>
+        <div class="containercad">
+            <form>
+                <li class="name">
+                    <?php
+                    echo "Nome: $nome"
+                    ?>
+                </li>
+                <li class="telefone">
+                    <?php
+                    echo "Telefone: $tell"
+                    ?>
+                </li>
+                <li class="doutor">
+                    <?php
+                    echo "Doutor/Doutora: $doctor"
+                    ?>
+                </li>
+                <li class="date">
+                    <?php
+                    echo "Data: $date"
+                    ?>
+                </li>
+            </form>
+        </div>
+        <p><a href="javascript:history.go(-1)">Voltar para a página anterior</a></p>
+    </div>
+</body>
+
+</html>
